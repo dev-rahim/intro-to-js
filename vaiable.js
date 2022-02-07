77eccfa33a985412454e8953ca11a31ea0c01841
dev-rahim <rahimuddin5915@gmail.com>
2022-02-07 20:54:26 +0600

    added update variable.js

--- vaiable.js
+++ vaiable.js
@@ -1,5 +1,13 @@
-var customer_name = 'Rahim Uddin';
-console.log( customer_name );
+// numeric
 var price = 30;
 var age = 35;
 var tempreture = 37;
+// String
+var customer_name = 'Rahim Uddin';
+console.log( customer_name );
+var address = 'balia vakutia, vakutia, sadar Jashore';
+// boolian
+var pass = true;
+var subscribe = false;
+
+
