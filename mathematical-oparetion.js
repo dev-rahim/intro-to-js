6192f94c49c89f389ff6e43e312ddfd3ec2ac573
dev-rahim <rahimuddin5915@gmail.com>
2022-02-07 21:18:49 +0600

    added update mathtetics

---
+++ mathematical-oparetion.js
@@ -0,0 +1,12 @@
+// summetion
+var onionPrice = 93;
+var eggPrice = 40;
+console.log( onionPrice + eggPrice );
+var totalPrice = ( 'Total price=' + ( onionPrice + eggPrice ) );
+console.log( totalPrice );
+var priceDefearrance = onionPrice - eggPrice;
+var priceMultiplication = onionPrice * eggPrice;
+console.log( 'price dearence is =' + priceDefearrance );
+console.log( 'price Multiplication is =' + priceMultiplication );
+
+
