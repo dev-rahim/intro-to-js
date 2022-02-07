612858f5972c096fea8b5e34d0f589c5139dc095
dev-rahim <rahimuddin5915@gmail.com>
2022-02-07 22:03:20 +0600

    added typeof.js

--- mathematical-oparetion.js
+++ mathematical-oparetion.js
@@ -9,4 +9,12 @@ var priceMultiplication = onionPrice * eggPrice;
 console.log( 'price dearence is =' + priceDefearrance );
 console.log( 'price Multiplication is =' + priceMultiplication );
 
-
+// Spacial edition
+var firstNumber = 0.1;
+var secondNumber = 0.2;
+var total = firstNumber + secondNumber;
+console.log( total );
+console.log( total.toFixed( 1 ) );
+console.log( typeof total.toFixed( 1 ) );
+console.log( parseFloat( total.toFixed( 1 ) ) );
+console.log( parseFloat( typeof total.toFixed( 1 ) ) );
---
+++ variable-typs.js
@@ -0,0 +1,10 @@
+var priceOfShirt = 100;
+console.log( typeof priceOfShirt );
+priceOfShirt = "100";
+console.log( typeof priceOfShirt );
+var isRaining = true;
+console.log( typeof isRaining );
+ var today=new Date();
+ console.log(today);
+ console.log(typeof today);
+
