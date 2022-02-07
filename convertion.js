efacc5778ac5c5bdf6cb5c9cab26b7d93b5374e4
dev-rahim <rahimuddin5915@gmail.com>
2022-02-07 21:44:36 +0600

    added convertion.js

---
+++ convertion.js
@@ -0,0 +1,15 @@
+// string  cancatetion
+var firstName = 'Rahim';
+var lastName = 'Uddin';
+var fullName = firstName + ' ' + lastName;
+console.log( fullName );
+
+var onionPrice = '42.52';
+var eggPrice = '33.33';
+// var totalPrice=parseInt(onionPrice) + parseInt(eggPrice);
+var totalPrice = parseInt( onionPrice ) + parseInt( eggPrice );
+console.log( totalPrice );
+var onionPriceNumber = parseFloat( onionPrice );
+console.log( onionPriceNumber );
+
+
