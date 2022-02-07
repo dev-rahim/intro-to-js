74e2c9d409a6ad534e2d5310d8cb7a9db4feae1a
dev-rahim <rahimuddin5915@gmail.com>
2022-02-07 21:05:13 +0600

    added update variable_name.js

---
+++ variable_name.js
@@ -0,0 +1,23 @@
+var name = 'tom bride'
+
+// reserved  keyword not allowed
+
+var true = 15;
+var delete=50;
+
+// quate is not allwed
+var 'jar' = 20;
+var my-ofice - address='kustia';
+var my_ofice_address = 'kustia';
+// use camale case 
+var myOficeAddress = 'kustia';
+
+var PIE = 3.1416;
+
+// first letter number not allowed
+
+var 99name = "fdsgfgsdh";
+
+// don't use meaningless name
+
+var dsds = 99;
